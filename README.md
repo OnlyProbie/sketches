4517f8f5ee0d459a551577b8c97accc30a73e0dd
OnlyProbie <onlyprobie@gmail.com>
2021-08-01 18:55:34 +0800

    improvement: update

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-### structure
-[[数组](./structure/数组.md)][[链表](./structure/链表.md)][[树](./structure/树.md)][[二叉树](./structure/二叉树.md)][[图](./structure/图.md)][[查找](./structure/查找.md)][[排序](./structure/排序.md)][[栈和队列](./structure/栈和队列.md)][[贪心算法](./structure/贪心算法.md)][[动态规划](./structure/动态规划.md)][[leetcode](./structure/leetcode.md)]
+# Sketches
 
-其他随记见issue
+Be a literary youth!
---
+++ docs/scripts/createpost.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# dir path
+path=./docs/_posts
+# current time
+date=$(date +%F)
+# get filename
+filename=$date"-"$1
+# get new file path
+createPath=$path/$filename
+# check folder
+if [ ! -d $path ]
+then
+  echo "The folder does not exist, please check!"
+else
+  if [ ! -f $path ]
+  then
+    # echo
+    echo $filename
+    echo "Create file "$createPath
+    # create file
+    touch $createPath
+    # echo content to file
+    echo "
+---
+layout: post
+title: Hello World                                # Title of the page
+hide_title: true                                  # Hide the title when displaying the post, but shown in lists of posts
+feature-img: "assets/img/sample.png"              # Add a feature-image to the post
+thumbnail: "assets/img/thumbnails/sample-th.png"  # Add a thumbnail image on blog view
+color: rgb(80,140,22)                             # Add the specified color as feature image, and change link colors in post
+bootstrap: true                                   # Add bootstrap to the page
+tags: [sample, markdown, html]
+---
+
+<!-- START doctoc -->
+<!-- END doctoc -->
+
+    " > $createPath
+
+    echo "File created successfully!"
+  else
+    echo "File already exists!"
+  fi
+fi
